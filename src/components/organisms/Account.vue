65e0bf277f8a2240f74a12ab2e98b8e135298f62
ma-ryu <maryu.janur@gmail.com>
2019-11-27 21:56:46 +0900

    fix

--- src/components/organisms/Account.vue
+++ src/components/organisms/Account.vue
@@ -1,58 +1,59 @@
 <template>
   <div class="account_page">
     <v-card>
-      <div class="secondary bg-color"></div>
-      <v-avatar size="100px" color="primary" class="avater">
-        <v-icon dark>mdi-account-circle</v-icon>
-      </v-avatar>
-      <v-card-title class="justify-center">User Name</v-card-title>
-      <v-card-subtitle class="justify-center">@User id</v-card-subtitle>
-      <v-card-title class="justify-center">User Info</v-card-title>
-      <v-card-text>
-        Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。
-        Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。
-        Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。
-      </v-card-text>
+      <div class="userProfile">
+        <v-avatar size="80px" color="primary" class="avater mt-5">
+          <v-icon dark>mdi-account-circle</v-icon>
+        </v-avatar>
+        <v-card-title class="justify-center">User Name</v-card-title>
+        <v-card-subtitle class="justify-center">@User id</v-card-subtitle>
+        <v-card-title class="justify-center">User Info</v-card-title>
+        <v-card-text class="caption">
+          Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。
+          Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。
+          Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。Userの説明が入ります。
+        </v-card-text>
+        <hr />
+        <v-card-actions class="mb-4">
+          <v-row class="mx-5 mt-10 justify-space-around">
+            <v-col md-4>
+              <v-list-item class="flex-column">
+                <div class="ansNumber mb-3">
+                  <p class="mt-4 display-1" style="color:blue">5</p>
+                </div>
+                <v-list-item-title class="subtitle-1">
+                  <v-icon size="20px" class="mr-2 pb-1">mdi-pencil</v-icon>Your
+                  Answers
+                </v-list-item-title>
+              </v-list-item>
+            </v-col>
+            <v-col md-4>
+              <v-list-item class="flex-column">
+                <div class="likeNumber mb-3">
+                  <p class="mt-4 display-1" style="color:blue">12</p>
+                </div>
+                <v-list-item-title class="subtitle-1">
+                  <v-icon size="20px" class="mr-2 pb-1">mdi-thumb-up</v-icon
+                  >Like Answers
+                </v-list-item-title>
+              </v-list-item>
+            </v-col>
+            <v-col md-4>
+              <v-list-item class="flex-column">
+                <div class="favNumber mb-3">
+                  <p class="mt-4 display-1" style="color:blue">10</p>
+                </div>
+                <v-list-item-title class="subtitle-1">
+                  <v-icon size="20px" class="mr-2 pb-1">mdi-heart</v-icon
+                  >Favorite Books
+                </v-list-item-title>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card-actions>
+      </div>
       <hr />
-      <v-card-actions class="mb-4">
-        <v-row class="mx-5 mt-10 justify-space-around">
-          <v-col md-4>
-            <v-list-item class="flex-column">
-              <div class="circle ansNumber mb-3">
-                <p class="mt-4 display-1">5</p>
-              </div>
-              <v-list-item-title class="title">
-                <v-icon size="25px" class="mr-2 pb-1">mdi-pencil</v-icon>Your
-                Answers
-              </v-list-item-title>
-            </v-list-item>
-          </v-col>
-          <v-col md-4>
-            <v-list-item class="flex-column">
-              <div class="circle likeNumber mb-3">
-                <p class="mt-4 display-1">12</p>
-              </div>
-              <v-list-item-title class="title">
-                <v-icon size="25px" class="mr-2 pb-1">mdi-thumb-up</v-icon>Like
-                Answers
-              </v-list-item-title>
-            </v-list-item>
-          </v-col>
-          <v-col md-4>
-            <v-list-item class="flex-column">
-              <div class="circle favNumber mb-3">
-                <p class="mt-4 display-1">10</p>
-              </div>
-              <v-list-item-title class="title">
-                <v-icon size="25px" class="mr-2 pb-1">mdi-heart</v-icon>Favorite
-                Books
-              </v-list-item-title>
-            </v-list-item>
-          </v-col>
-        </v-row>
-      </v-card-actions>
-      <hr />
-      <v-card-title>Your Profile</v-card-title>
+      <v-card-title class="justify-center">Your Profile</v-card-title>
       <div class="userName">
         <v-card-subtitle>User Name</v-card-subtitle>
         <v-card-text>User Nameが入ります。</v-card-text>
@@ -74,26 +75,4 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class Account extends Vue {}
 </script>
 
-<style scoped>
-v-card {
-  border-radius: 0;
-}
-.bg-color {
-  height: 200px;
-  width: 100%;
-}
-.avater {
-  margin-top: -50px;
-}
-.circle {
-  height: 75px;
-  width: 75px;
-  border-radius: 50%;
-  border: 1px solid rgb(0, 0, 0);
-}
-hr {
-  color: grey;
-  display: inline-block;
-  width: 95%;
-}
-</style>
+<style lng="scss" scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -36,7 +36,7 @@ const routes = [
       {
         path: "account",
         name: "account",
-        component: () => import("@/components/organisms/Account.vue")
+        component: () => import("@/views/AccountPage.vue")
       },
       {
         path: "search",
