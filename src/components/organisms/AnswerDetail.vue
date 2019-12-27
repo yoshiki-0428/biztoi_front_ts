bec1f287b998fe38626f44c63259f45edbec8fd2
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2019-12-27 13:33:27 +0900

    [add] 回答詳細画面に質問に応じた回答を表示するように修正（UIは一旦現状でPush）

--- src/components/organisms/AnswerDetail.vue
+++ src/components/organisms/AnswerDetail.vue
@@ -1,29 +1,25 @@
 <template>
   <v-expansion-panels multiple accordion v-model="panel">
-    <v-expansion-panel>
+    <v-expansion-panel v-for="(s, index) in stepMap" :key="index">
       <v-expansion-panel-header class="title">
-        Step 1
+        Step {{ s.no }} {{ s.name }}
       </v-expansion-panel-header>
       <v-expansion-panel-content>
-        {{ answerHead }}
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-
-    <v-expansion-panel>
-      <v-expansion-panel-header class="title">
-        Step 2
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        Some content
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-
-    <v-expansion-panel>
-      <v-expansion-panel-header class="title">
-        Step 3
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        Some content
+        <v-list-item
+          three-line
+          v-for="(q, index) in filterdQuestions(s.no)"
+          :key="index"
+        >
+          <!-- TODO UIの整理、調整  -->
+          <v-list-item-content>
+            <v-list-item-subtitle>Q. {{ q.title }}</v-list-item-subtitle>
+            <v-list-item-subtitle
+              v-for="(a, index) in filterdAnswers(q.id)"
+              :key="index"
+              >A. {{ a.answer }}</v-list-item-subtitle
+            >
+          </v-list-item-content>
+        </v-list-item>
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
@@ -31,18 +27,25 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import { Answer, AnswerHead } from "@/axios/biztoi";
+import { Answer, AnswerHead, Question } from "@/axios/biztoi";
+import isUndefined from "lodash/isUndefined";
 
 @Component
 export default class AnswerDetail extends Vue {
   @Prop({ default: null }) private answerHead!: AnswerHead;
+  @Prop({ default: () => [] }) private questionList!: Question[];
+  private stepMap: { no: number; name: string }[] = [
+    { no: 1, name: "目的" },
+    { no: 2, name: "知識" },
+    { no: 3, name: "行動プラン" }
+  ];
   private panel: number[] = [0, 1, 2];
-  // TODO 質問取得して質問のステップに紐づくAnswerを収集する
-  // get stepToAnswer(step: string): Answer[] {
-  //   if (this.answerHead.answers) {
-  //     return this.answerHead.answers.filter(answer => answer.)
-  //
-  //   }
-  // }
+  private filterdQuestions(step: string): Question[] {
+    return this.questionList.filter(q => q.step === Number(step));
+  }
+  private filterdAnswers(questionId: string): Answer[] {
+    if (isUndefined(this.answerHead.answers)) return [];
+    return this.answerHead.answers!.filter(a => a.questionId === questionId);
+  }
 }
 </script>
--- src/container/AnswerDetail.ts
+++ src/container/AnswerDetail.ts
@@ -2,17 +2,23 @@ import { connect } from "vuex-connect";
 import { answerShareModule } from "@/store/AnswerStore";
 import router from "@/router";
 import AnswerDetail from "@/components/organisms/AnswerDetail.vue";
-import { AnswerHead } from "@/axios/biztoi";
+import { AnswerHead, Question } from "@/axios/biztoi";
+import { questionModule } from "@/store/QuestionModule";
 
 export default connect({
   stateToProps: {
-    answerHead: (): AnswerHead => answerShareModule.answerHead
+    answerHead: (): AnswerHead => answerShareModule.answerHead,
+    questionList: (): Question[] => questionModule.questionList
   },
   lifecycle: {
-    created: async () =>
+    created: async () => {
       answerShareModule.getAnswerHead({
         bookId: router.currentRoute.params.bookId,
         answerId: router.currentRoute.params.answerHeadId
-      })
+      });
+      questionModule.getQuestionList({
+        bookId: router.currentRoute.params.bookId
+      });
+    }
   }
 })("answer-detail", AnswerDetail);
