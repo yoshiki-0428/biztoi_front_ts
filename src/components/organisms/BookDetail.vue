c61e0ae798431017f9bcdb920ecee7e7e35b20c2
ma-ryu <maryu.janur@gmail.com>
2020-01-20 20:04:38 +0900

    サイズ調整

--- src/components/organisms/BookDetail.vue
+++ src/components/organisms/BookDetail.vue
@@ -1,47 +1,43 @@
 <template>
   <v-card class="mb-4">
-    <v-row>
-      <v-col cols="4" class="mt-3 ml-2 pa-1">
-        <v-img
-          :src="book.pictureUrl"
-          max-height="200"
-          contain
-          class="ma-1"
-        ></v-img>
+    <v-row justify="center" dense>
+      <v-col cols="4" class="pa-2">
+        <v-img :src="book.pictureUrl" contain></v-img>
       </v-col>
-      <v-col cols="7" class="pb-0">
-        <div class="text-left">
+      <v-col cols="8" class="d-flex flex-column pa-0" align="">
+        <v-card-text class="text-left pa-0">
           <v-card-title
             v-text="book.title"
-            class="font-weight-bold subtitle-2 pt-1"
+            class="font-weight-bold subtitle-2 pa-1"
           ></v-card-title>
-        </div>
-        <div class="text-left">
-          <v-card-title
-            v-text="getAuthor(book.author)"
-            class="subtitle-2 pt-1"
-          ></v-card-title>
-        </div>
-        <div class="text-left">
-          <v-card-title
-            v-text="getCategory(book.category)"
-            class="subtitle-2 pt-1"
-          ></v-card-title>
-        </div>
-        <div class="text-left ml-2">
+        </v-card-text>
+        <v-card-text class="text-left pa-0">
+          <v-card-title class="caption pa-1">
+            <v-icon class="mr-2" color="black" size="15">fa-pencil-alt</v-icon>
+            {{ getAuthor(book.author) }}
+          </v-card-title>
+        </v-card-text>
+        <v-card-text class="text-left pa-0">
+          <v-card-title class="caption pa-1">
+            <v-icon class="mr-2" color="black" size="15">fa-book</v-icon>
+            {{ getCategory(book.category) }}
+          </v-card-title>
+        </v-card-text>
+        <v-spacer></v-spacer>
+        <v-card-actions>
+          <v-spacer></v-spacer>
           <v-btn color="primary" class="font-weight-bold" :href="book.linkUrl">
             書誌情報
           </v-btn>
-        </div>
+          <v-btn icon @click="toggleIsActive">
+            <v-icon :color="getColor">mdi-heart</v-icon>
+          </v-btn>
+          <share-icon-button
+            :text="`BizToiアプリで${book.title}の本詳細を見る`"
+          />
+        </v-card-actions>
       </v-col>
     </v-row>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn icon @click="toggleIsActive">
-        <v-icon :color="getColor">mdi-heart</v-icon>
-      </v-btn>
-      <share-icon-button :text="`BizToiアプリで${book.title}の本詳細を見る`" />
-    </v-card-actions>
   </v-card>
 </template>
 
