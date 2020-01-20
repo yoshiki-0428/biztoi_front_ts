ecdba8c97f307e96f758442924041eeeaa2468c0
ma-ryu <maryu.janur@gmail.com>
2020-01-21 00:33:34 +0900

    btn修正 画像の表示修正

--- src/components/organisms/BookDetail.vue
+++ src/components/organisms/BookDetail.vue
@@ -1,33 +1,43 @@
 <template>
   <v-card class="mb-4">
     <v-row justify="center" dense>
-      <v-col cols="4" class="pa-2">
-        <v-img :src="book.pictureUrl" contain></v-img>
+      <v-col cols="4" class="pa-0">
+        <v-img class="radious" :src="book.pictureUrl"></v-img>
       </v-col>
-      <v-col cols="8" class="d-flex flex-column pa-0" align="">
+      <v-col cols="8" class="d-flex flex-column pa-0 pl-1">
         <v-card-text class="text-left pa-0">
           <v-card-title
             v-text="book.title"
             class="font-weight-bold subtitle-2 pa-1"
           ></v-card-title>
         </v-card-text>
-        <v-card-text class="text-left pa-0">
+        <v-card-text
+          v-if="getAuthor(book.author) !== ''"
+          class="text-left pa-0"
+        >
           <v-card-title class="caption pa-1">
-            <v-icon class="mr-2" color="black" size="15">fa-pencil-alt</v-icon>
+            <v-icon class="mr-2" color="black" size="15">
+              fa-pencil-alt
+            </v-icon>
             {{ getAuthor(book.author) }}
           </v-card-title>
         </v-card-text>
-        <v-card-text class="text-left pa-0">
+        <v-card-text
+          v-if="getCategory(book.category) !== ''"
+          class="text-left pa-0"
+        >
           <v-card-title class="caption pa-1">
-            <v-icon class="mr-2" color="black" size="15">fa-book</v-icon>
+            <v-icon class="mr-2" color="black" size="15">
+              fa-book
+            </v-icon>
             {{ getCategory(book.category) }}
           </v-card-title>
         </v-card-text>
         <v-spacer></v-spacer>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" class="font-weight-bold" :href="book.linkUrl">
-            書誌情報
+          <v-btn icon :href="book.linkUrl">
+            <v-icon>fa-share</v-icon>
           </v-btn>
           <v-btn icon @click="toggleIsActive">
             <v-icon :color="getColor">mdi-heart</v-icon>
@@ -68,3 +78,9 @@ export default class BookOverView extends Vue {
   private onClick(sendLikeInfo: SendLikeInfo) {}
 }
 </script>
+<style scoped>
+.v-image {
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+}
+</style>
