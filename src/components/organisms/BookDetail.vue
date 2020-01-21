ee257b3168eb6e08612c092274f591f422534718
ma-ryu <maryu.janur@gmail.com>
2020-01-21 18:42:29 +0900

    画像の高さをカード枠最大になるように修正

--- src/components/organisms/BookDetail.vue
+++ src/components/organisms/BookDetail.vue
@@ -2,13 +2,13 @@
   <v-card class="mb-4">
     <v-row dense>
       <v-col cols="4" class="pa-0 pl-1">
-        <v-img :src="book.pictureUrl" />
+        <v-img height="100%" :src="book.pictureUrl" />
       </v-col>
       <v-col cols="8" class="d-flex flex-column pa-0 pl-1">
         <v-card-text class="text-left pa-0">
           <v-card-title
             v-text="book.title"
-            class="font-weight-bold subtitle-2 pa-1"
+            class="font-weight-bold subtitle-1 pa-1"
           ></v-card-title>
         </v-card-text>
         <v-card-text
@@ -16,7 +16,7 @@
           class="text-left pa-1"
         >
           <v-card-title class="caption pa-0">
-            <v-icon class="mr-2" color="black" size="15">
+            <v-icon class="mr-2" color="black" size="12">
               fa-pencil-alt
             </v-icon>
             {{ getAuthor(book.author) }}
@@ -27,7 +27,7 @@
           class="text-left pa-1"
         >
           <v-card-title class="caption pa-0">
-            <v-icon class="mr-2" color="black" size="15">
+            <v-icon class="mr-2" color="black" size="12">
               fa-book
             </v-icon>
             {{ getCategory(book.category) }}
@@ -68,17 +68,17 @@ export default class BookOverView extends Vue {
     this.book.favorite = !this.book.favorite;
     this.onClick({ id: this.book.id, active: this.book.favorite });
   }
-  private getAuthor(author: string[]): string {
+  private getAuthor(author: string[]): string | undefined | null {
     return author.join();
   }
-  private getCategory(category: string[]): string {
+  private getCategory(category: string[]): string | undefined | null {
     return category.join();
   }
   @Emit("on-click-favorite")
   private onClick(sendLikeInfo: SendLikeInfo) {}
 }
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .v-image {
   border-top-left-radius: 4px;
   border-bottom-left-radius: 4px;
