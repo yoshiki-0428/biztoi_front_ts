1e7417e5ab32f2b91ca18b0d7deb7371d0d9a1dd
ma-ryu <maryu.janur@gmail.com>
2020-02-21 01:27:03 +0900

    パンくず作成中

---
+++ src/components/organisms/Breadcrumb.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-breadcrumbs :items="items" class="pl-0 pt-0">
+      <template v-slot:item="{ item }">
+        <v-breadcrumbs-item exact :disabled="item.disabled" :to="item.path">
+          {{ item.name }}
+        </v-breadcrumbs-item>
+      </template>
+    </v-breadcrumbs>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import router from "@/router";
+
+export interface IBreadcrumbs {
+  name: string;
+  path: string;
+  disabled: boolean;
+}
+@Component
+export default class Breadcrumb extends Vue {
+  // private paths: string[] = router.currentRoute.path.split("/");
+  @Prop({ default: () => [] })
+  private items!: IBreadcrumbs[];
+}
+</script>
--- src/views/BookDetailPage.vue
+++ src/views/BookDetailPage.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+    <breadcrumb :items="paths" />
     <book-detail />
     <toi-card />
     <answer-over-view-list />
@@ -7,17 +8,35 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from "vue-property-decorator";
 import BookDetail from "@/container/BookDetail";
 import ToiCard from "@/container/ToiCard";
 import AnswerOverViewList from "@/container/AnswerOverViewList";
+import Breadcrumb, {
+  IBreadcrumbs
+} from "@/components/organisms/Breadcrumb.vue";
+import router from "@/router";
 
 @Component({
   components: {
     AnswerOverViewList,
     ToiCard,
-    BookDetail
+    BookDetail,
+    Breadcrumb
   }
 })
-export default class BookDetailPage extends Vue {}
+export default class BookDetailPage extends Vue {
+  private paths: IBreadcrumbs[] = [
+    {
+      name: "top",
+      path: "/top",
+      disabled: false
+    },
+    {
+      name: router.currentRoute.params.bookId,
+      path: `/top/book/${router.currentRoute.params.bookId}`,
+      disabled: false
+    }
+  ];
+}
 </script>
