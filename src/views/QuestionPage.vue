44138e8e2d3b51ec8e1615ec9f50750cc3e27963
ma-ryu <maryu.janur@gmail.com>
2020-02-25 23:01:43 +0900

    パンくず作成（QuestionPage）

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,17 +1,42 @@
 <template>
-  <div>
+  <v-container>
+    <breadcrumb :items="paths" />
     <answer-input />
-  </div>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import AnswerInput from "@/container/AnswerInput";
+import Breadcrumb, {
+  IBreadcrumbs
+} from "@/components/organisms/Breadcrumb.vue";
+import router from "@/router";
 
 @Component({
   components: {
-    AnswerInput
+    AnswerInput,
+    Breadcrumb
   }
 })
-export default class QuestionPage extends Vue {}
+export default class QuestionPage extends Vue {
+  // ToDo URL ex(/top/book/bookId/toi/questions/questionId)
+  private paths: IBreadcrumbs[] = [
+    {
+      name: "top",
+      path: "/top",
+      disabled: false
+    },
+    {
+      name: router.currentRoute.params.bookId,
+      path: `/top/book/${router.currentRoute.params.bookId}`,
+      disabled: false
+    },
+    {
+      name: router.currentRoute.params.questionId,
+      path: `/top/book/toi/questions/${router.currentRoute.params.questionId}`,
+      disabled: false
+    }
+  ];
+}
 </script>
