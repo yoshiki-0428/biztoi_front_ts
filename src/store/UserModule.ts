d06cd33c6e8286682a3d9b57781a9680f1e0a806
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-03-28 00:00:48 +0900

    [fix] UserAPIと通信するように修正

--- src/store/UserModule.ts
+++ src/store/UserModule.ts
@@ -23,18 +23,10 @@ class UserModule extends VuexModule {
 
   @Action
   public async getUserInfo() {
-    // TODO API定義変更する code https://stoplight.io/p/studio/gh/yoshiki-0428/biztoi_openapi
     const res: AxiosResponse<BizToiUser> = await baseApi.userInfo("");
-    this.SET_USER_INFO({
-      id: "4bd0d7cc-df1d-4cb6-b804-793f6b506273",
-      email: "biztoi@biztoi.com",
-      pictureUrl: "https://picsum.photos/100/100",
-      nickname: "biz",
-      country: "ja"
-    });
 
     if (res.data) {
-      // this.SET_USER_INFO(res.data);
+      this.SET_USER_INFO(res.data);
       return;
     }
   }
