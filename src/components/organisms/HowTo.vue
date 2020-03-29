0401b7a80fad75321cb49e43568ffa16b7e59c78
ma-ryu <maryu.janur@gmail.com>
2020-03-29 13:20:02 +0900

    fix

--- src/components/organisms/HowTo.vue
+++ src/components/organisms/HowTo.vue
@@ -1,60 +1,59 @@
 <template>
   <v-list class="pa-5" height="80vh">
     <v-list-item-title class="display-1 my-10">ご利用方法</v-list-item-title>
-    <v-list-item class="text-left">
-      <v-list-item-avatar class="primary darken-1">1</v-list-item-avatar>
+    <v-list-item class="text-left" v-for="item in step" :key="item.step">
+      <v-list-item-avatar class="primary darken-1">
+        {{ item.stepNum }}
+      </v-list-item-avatar>
       <v-list-item-content class="ma-5">
         <v-list-item-title>
-          ご登録
-          <v-icon size="15" class="ml-2 mb-1">fa-sign-in-alt</v-icon>
+          {{ item.title }}
+          <v-icon size="15" class="ml-2 mb-1">{{ item.icon }}</v-icon>
         </v-list-item-title>
         <v-list-item-subtitle class="caption">
-          新規登録ボタンより登録ください。
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-    <v-list-item class="text-left">
-      <v-list-item-avatar class="primary darken-1">2</v-list-item-avatar>
-      <v-list-item-content class="ma-5">
-        <v-list-item-title>
-          本の検索
-          <v-icon size="15" class="ml-2 mb-1">fa-search</v-icon>
-        </v-list-item-title>
-        <v-list-item-subtitle class="caption">
-          本を選びましょう！
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-    <v-list-item class="text-left">
-      <v-list-item-avatar class="primary darken-1">3</v-list-item-avatar>
-      <v-list-item-content class="ma-5">
-        <v-list-item-title>
-          回答
-          <v-icon size="15" class="ml-2 mb-1">fa-pen</v-icon>
-        </v-list-item-title>
-        <v-list-item-subtitle class="caption">
-          STEPに沿って回答してみよう！
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-    <v-list-item class="text-left">
-      <v-list-item-avatar class="primary darken-1">4</v-list-item-avatar>
-      <v-list-item-content class="ma-5">
-        <v-list-item-title>
-          共有
-          <v-icon size="15" class="ml-2 mb-1">fa-book-open</v-icon>
-        </v-list-item-title>
-        <v-list-item-subtitle class="caption">
-          他の人の回答も見てみよう！
+          {{ item.text }}
         </v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
   </v-list>
 </template>
 
-<script>
+<script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+interface IStep {
+  stepNum: number;
+  title: string;
+  icon: string;
+  text: string;
+}
 @Component
-export default class HowTo extends Vue {}
+export default class HowTo extends Vue {
+  step: Array<IStep> = [
+    {
+      stepNum: 1,
+      title: "ご登録",
+      icon: "fa-sign-in-alt",
+      text: "新規登録ボタンより登録ください。"
+    },
+    {
+      stepNum: 2,
+      title: "本の検索",
+      icon: "fa-search",
+      text: "本を選びましょう！"
+    },
+    {
+      stepNum: 3,
+      title: "回答",
+      icon: "fa-pen",
+      text: "STEPに沿って回答してみよう！"
+    },
+    {
+      stepNum: 4,
+      title: "共有",
+      icon: "fa-book-open",
+      text: "他の人の回答も見てみよう！"
+    }
+  ];
+}
 </script>
