2a3a92746ed3d174526d09ddb18755544d3d44ae
ma-ryu <maryu.janur@gmail.com>
2020-04-05 20:18:14 +0900

    hotjar追加

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import router from "./router";
 import store from "./store";
 import { vuetify } from "./plugins/vuetify";
 import VueAnalytics from "vue-analytics";
+// @ts-ignore
+import Hotjar from "vue-hotjar";
 import "./plugins/axios";
 
 Vue.config.productionTip = false;
@@ -12,6 +14,11 @@ Vue.config.productionTip = false;
 Vue.use(VueAnalytics, {
   id: process.env.VUE_APP_GA_ID,
   router
+});
+
+Vue.use(Hotjar, {
+  id: process.env.VUE_APP_HOTJAR_ID,
+  isProduction: true
 });
 
 new Vue({
