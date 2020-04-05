b2dc07efd0e21e627f54783b35fbc7fe22cc027a
ma-ryu <maryu.janur@gmail.com>
2020-04-05 17:48:00 +0900

    google analytics追加

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import { vuetify } from "./plugins/vuetify";
+import VueAnalytics from "vue-analytics";
 import "./plugins/axios";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: "UA-162848676-1",
+  router
+});
 
 new Vue({
   vuetify,
