c779d4733ce71eed668410c4fda18caf1c963489
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-04-10 12:43:22 +0900

    [fix] logout 修正

--- src/components/organisms/Account.vue
+++ src/components/organisms/Account.vue
@@ -51,16 +51,14 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { BizToiUser } from "@/axios/biztoi";
-import axios from "axios";
-import router from "@/router";
 
 @Component
 export default class Account extends Vue {
   @Prop({ default: {} }) private userInfo!: BizToiUser;
   private logout() {
-    axios
-      .post(`${process.env.VUE_APP_API_BASE_URL}/logout`)
-      .finally(() => router.push("/"));
+    window.location.href =
+      process.env.VUE_APP_COGNITO_LOGOUT_URI +
+      process.env.VUE_APP_API_LOGOUT_URI;
   }
 }
 </script>
